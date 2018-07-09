6d194e1e42719c24de0ee893dd6945eb2d77b460
IAFDI <magdalenaaprice@gmail.com>
2018-07-09 16:15:23 -0700

    second commit

--- app/src/main/java/me/maprice/parsetagram/HomeActivity.java
+++ app/src/main/java/me/maprice/parsetagram/HomeActivity.java
@@ -2,6 +2,15 @@ package me.maprice.parsetagram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
+
+import me.maprice.parsetagram.model.Post;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +18,20 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ParseQuery.getQuery(Post.class).findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if (e == null){
+                    for (int i = 0; i < objects.size(); i++){
+                        Log.d("HomeActivity", "Post[" +i +"] =" + objects.get(i).getDescription());
+                    }
+
+                }else{
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/me/maprice/parsetagram/ParseApp.java
+++ app/src/main/java/me/maprice/parsetagram/ParseApp.java
@@ -4,13 +4,16 @@ import android.app.Application;
 import android.content.res.Configuration;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
+
+import me.maprice.parsetagram.model.Post;
 
 public class ParseApp extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-
+        ParseObject.registerSubclass(Post.class);
         final Parse.Configuration config = new Parse.Configuration.Builder(this)
                 .applicationId("petrichor")
                 .clientKey("rider-rider-pumpkin-spider")
